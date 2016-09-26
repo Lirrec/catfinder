2b70db4eddceed6d6b57b09a29ff59f79b4ad913
jgerauer <jgerauer@haw-landshut.de>
2016-09-26 16:57:42 +0200

    Added more eventCallback functionality, now allowing to create callbacks for any event. Currently still using a single default callback function that'll write an entry into a membered list whenever a specified event happens. Also cleaned up unneeded member variables.

--- catfinder_ue4/Source/catfinder_ue4/NAOSession.cpp
+++ catfinder_ue4/Source/catfinder_ue4/NAOSession.cpp
@@ -221,27 +221,34 @@ void UNAOSession::getTemperatures() {
 	}
 }
 
-//creates a callback for testing purposes, triggers when the naos chestbutton is pressed
-void UNAOSession::createCallbackTest(FString eventName) {
+//Creates a callback for a specified event that will add an entry to a member-List containing the name of the event.
+//For a full list of possible events, see http://doc.aldebaran.com/2-1/naoqi-eventindex.html
+void UNAOSession::createCallback(FString eventName) {
 	try {
-		sigSub = qi::SignalSubscriber(qi::AnyFunction::fromDynamicFunction(boost::bind(&UNAOSession::testCallback, this, _1)));
 		qi::AnyObject alm = session->service("ALMemory");
-		callbackBuffer = alm.call<qi::AnyObject>("subscriber", "ChestButtonPressed");
-		f = callbackBuffer.connect("signal", sigSub);
-		f.wait();
-		if (f.hasError()) {
-			UE_LOG(LogTemp, Error, TEXT("Error on signal connect: '%s'"), ANSI_TO_TCHAR(f.error().c_str()));
+		qi::AnyObject subscriber = alm.call<qi::AnyObject>("subscriber", TCHAR_TO_ANSI(*eventName));
+		//Create a SignalSubscriber that will handle callbacks From NAO
+		qi::SignalSubscriber sigSub(qi::AnyFunction::fromDynamicFunction(boost::bind(&UNAOSession::eventCallback, this, eventName, _1)));
+		//The signal created by the subscribe function will Always be called 'signal'
+		qi::Future<qi::SignalLink> futureLink = subscriber.connect("signal", sigSub);
+
+		futureLink.wait();
+		if (futureLink.hasError()) {
+			UE_LOG(LogTemp, Error, TEXT("Error on Signal connect: '%s'"), ANSI_TO_TCHAR(futureLink.error().c_str()));
 		}
-		UE_LOG(LogTemp, Error, TEXT("signal connected"));
+		//If the subscriber object isn't stored, the callback will no longer happen
+		subscriberList.push_back(subscriber);
+		UE_LOG(LogTemp, Warning, TEXT("Signal for Event '%s' connected"), *eventName);
 	}
 	catch (std::exception& e) {
 		UE_LOG(LogTemp, Warning, TEXT("QI Exception: %s"), ANSI_TO_TCHAR(e.what()));
 	}
 }
 
-//simple callback, writing message to UE-Logs when called
-qi::AnyReference UNAOSession::testCallback(const std::vector<qi::AnyReference>& params) {
-	UE_LOG(LogTemp, Warning, TEXT("NAOevent: CHESTBUTTON PRESSED"));
+//simple callback, writing message to UE-Logs and adding the event to a membered list when called
+qi::AnyReference UNAOSession::eventCallback(FString eventName, const std::vector<qi::AnyReference>& params) {
+	UE_LOG(LogTemp, Warning, TEXT("NAOEvent: '%s'"),*eventName);
+	eventMap.insert(std::make_pair(std::string(TCHAR_TO_UTF8(*eventName)), params));
 	//returntype needs to be anyreference. this solution was taken form official libqi examples
-	return qi::AnyReference();// nullptr;
+	return qi::AnyReference();
 }
